f7ecf8abc2ae1d77ae96dbcf94bd599e0265635c
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-08 14:59:09 +0500

    .js modified

--- JavaScript Practice/2.Variables and Datatypes/index.js
+++ JavaScript Practice/2.Variables and Datatypes/index.js
@@ -1,22 +1,43 @@
+
+// var
+// var is global scoped and function scoped but not block scoped
+// var can be redeclaired that may create issues
+// *************************
+
 // var age = 25;
 
 // if(true){
 //     console.log(age);
 // }
 
-// var is global scoped and function scoped but not block scoped
-// var can be redeclaired that may create issues
+// var age = 25;
+// var age = 28;
 
-var age = 25;
-var age = 28;
+// console.log(age);
 
-console.log(age);
-
+// **********************
 // to avoid this issue 
 // we can use "let", it is blocked scope
 // it can't be redeclare but manipulate.
+// **********************
 
-let fname = "shafaq";
+// let fname = "shafaq";
 // console.log(fname);
-// let fname = "zainab";
-console.log(fname);
+// let fname = "zainab"; --> will throw error
+// fname = "zainab" --> can be updated but can't redeclear
+// console.log(fname);
+
+// **********************
+// Constant
+// can't be updated nor redeclared
+// **********************
+
+const a = 35;
+console.log(a);
+// a = "shafaq"; --> will throw error
+// console.log(a)
+// const a = "shafaq"; --> will throw error
+// console.log(a);
+
+
+
