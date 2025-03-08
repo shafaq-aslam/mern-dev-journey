897a8b361e6099250c66be80f123233a2b02d93b
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-08 12:18:09 +0500

    index.js file added

---
+++ JavaScript Practice/2.Variables and Datatypes/index.js
@@ -0,0 +1,13 @@
+// var age = 25;
+
+// if(true){
+//     console.log(age);
+// }
+
+// var is global scoped and function scoped but not block scoped
+// var can be redeclaired that may create issues
+
+var age = 25;
+var age = 28;
+
+console.log(age);
