76a3a4a6b32035df61ae42efbbe503f365b2fb32
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-09 21:59:51 +0500

    unary operators added

---
+++ JavaScript Practice/3.Operators and Conditions/uninary_operators.js
@@ -0,0 +1,25 @@
+let num1 = 26;
+let num2 = 5;
+
+// Unary Operators
+console.log(num1--); // 26
+console.log(num1); // 25
+console.log(--num1); // 24
+console.log(num1); // 24
+
+console.log(num2++); // 5
+console.log(num2); // 6
+console.log(++num2); // 7
+console.log(num2) // 7
+
+console.log(num1--); // 26
+console.log(++num1); // 26
+console.log(num1) // 26
+console.log(num1++) // 26
+console.log(--num1); // 26
+console.log(num1); // 26
+
+
+
+
+
