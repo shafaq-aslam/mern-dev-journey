5f23dd0c4b803941eb163184bfab439790c286e4
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-09 21:47:33 +0500

    arithmetic operators topic added

---
+++ JavaScript Practice/3.Operators and Conditions/index.js
@@ -0,0 +1,14 @@
+let num1 = 6;
+let num2 = 8;
+
+// Arithmetic Operators
+let add = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+let mod = num1 % num2;
+let pow = num1**num2;
+
+console.log("Addition is: ", add, "\nSubtraction is: ", sub, "\nMultiplication is: " ,mult);
+
+console.log("Division is: ", div, "\nModulus is: ", mod, "\nPower of num2 is: " ,pow);
