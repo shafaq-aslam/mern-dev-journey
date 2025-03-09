38be0e77cdf8a5df24e8ca5ea19e34f34923fdde
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-09 22:16:53 +0500

    assignment operators added

---
+++ JavaScript Practice/3.Operators and Conditions/assignment_operators.js
@@ -0,0 +1,10 @@
+// Assignment Operators
+// =, +=, -=, *=, /=, %=, **=
+
+let num = 5;
+console.log(num+=2); // 7
+console.log(num-=1); // 6
+console.log(num*=3); // 18
+console.log(num**=5); // 1,889,568
+console.log(num/=2); // 944,784
+console.log(num%=2); // 0
