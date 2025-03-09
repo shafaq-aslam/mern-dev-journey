c1aab77470ae52e0e173617fdbc5ea00034f00d9
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-09 22:47:23 +0500

    comparison operators added

---
+++ JavaScript Practice/3.Operators and Conditions/comparison_operators.js
@@ -0,0 +1,10 @@
+
+// Comparison Operators
+console.log('5' < 5); // false
+console.log('5' >= 5); // true
+console.log(8 > 7); // true
+console.log(8 <= 7); // false
+console.log(5 == '5'); // true
+console.log(5 == '15'); // false
+console.log(5 === 15); // false
+console.log('15' === '15'); // true
