e55066e5257085fcc565460d12c706cf6f99d360
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-10 10:58:33 +0500

    ternary operators added

---
+++ JavaScript Practice/3.Operators and Conditions/ternary_operators.js
@@ -0,0 +1,8 @@
+
+let age = 21;
+let ageChecker = (age>18) ? "You can vote" : "You can't vote";
+console.log(ageChecker);
+
+let height = 6;
+let heightChecker = (height>=6) ? "You can apply for the game" : "You can't apply for the game";
+console.log(heightChecker)
