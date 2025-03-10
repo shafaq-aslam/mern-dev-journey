42ade1805ca8eab3143c0674ca13a2d860867f4d
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-10 21:09:47 +0500

    if else if added

--- JavaScript Practice/3.Operators and Conditions/if_else.js
+++ JavaScript Practice/3.Operators and Conditions/if_else.js
@@ -5,3 +5,13 @@ if(age >= 18){
 } else{
     console.log("Can't vote");
 }
+//*************************************
+let num = 0;
+
+if(num > 0){
+    console.log('Number is positive');
+} else if(num < 0){
+    console.log('Number is negative');
+} else{
+    console.log('Number is zero');
+}
