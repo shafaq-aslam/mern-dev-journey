2a2e98c5cc952d8b7bbf6b39fcf9c3d23b97eb73
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-10 21:26:01 +0500

    switch case added

---
+++ JavaScript Practice/3.Operators and Conditions/switch_case.js
@@ -0,0 +1,43 @@
+let day = 'Friday';
+
+// switch(day){
+//     case 'Monday':
+//         console.log('This is the start of week');
+//         break;
+//     case 'Tuesday':
+//         console.log('Second day of week');
+//         break;
+//     case 'Wednesday':
+//         console.log('Third day of week');
+//         break;
+// }
+
+switch(day){
+    case 'Monday':
+    case 'moonday':
+        console.log('This is the start of week');
+        break;
+    case 'Tuesday':
+    case 'tuesday':
+        console.log('Second day of week');
+        break;
+    case 'Wednesday':
+    case 'wednesday':
+        console.log('Third day of week');
+        break;
+    case 'Thursday':
+    case 'thursday':
+        console.log('Forth day of week');
+         break;
+    case 'Friday':
+    case 'friday':
+        console.log('Fifth day of week');
+         break;
+    case 'Saturday':
+    case 'saturday':
+        console.log('six day of week');
+         break;
+    default:
+        console.log('Last day of the week');
+         break;
+}
