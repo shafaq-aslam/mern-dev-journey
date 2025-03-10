9cb89019aefb953fa9ceb6fa4bbe0a40b1e9734c
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-10 11:28:33 +0500

    logical operators added

---
+++ JavaScript Practice/3.Operators and Conditions/logical_operators.js
@@ -0,0 +1,5 @@
+console.log(true && false); // false
+console.log(true && true); // true
+console.log(true && true && false); // false
+console.log(true && true && true); // true
+console.log((true && true) && true); // true
