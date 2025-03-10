ccd83e717acb8e81a1deedd83c7b6af3d2b5d527
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-10 14:11:18 +0500

    bitwise operators updated

---
+++ JavaScript Practice/3.Operators and Conditions/bitwise_operators.js
@@ -0,0 +1,12 @@
+console.log(5 & 7); // 5
+console.log(6 | 6); // 6
+console.log(5 & 9); // 1
+console.log(2 | 7); // 7
+console.log(2 ^ 5); // 7
+console.log(7 ^ 8); // 15
+console.log(~(1)); // -2
+console.log(5<<2); // 20
+console.log(5<<4); // 80
+console.log(5<<1); // 10
+console.log(10>>1); // 5
+console.log(15>>2); // 3.75
