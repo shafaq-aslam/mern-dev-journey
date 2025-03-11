27040042a8b6df952cee5225430f9fef1916481e
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-11 12:00:33 +0500

    for loop modified

--- JavaScript Practice/4.Loops and Strings/for_loop.js
+++ JavaScript Practice/4.Loops and Strings/for_loop.js
@@ -1,3 +1,14 @@
 for(let i = 1; i <= 10; i +=1){
     console.log("Shafaq");
 }
+
+let num = 5;
+for(let i = 1; i <= 10; i+=1){
+    console.log(num, " x ", i, " = ", num*i);
+    // console.log("2", " x ", i, " = ", 2*i);
+}
+
+//Reverse loop
+for(let i=10; i>=1; i--){
+    console.log("5 ", " x ", i, " = ", 5*i);
+}
