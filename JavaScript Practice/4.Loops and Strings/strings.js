5ec65bbfaf589656c441722cb55a64492a660c89
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-12 16:29:36 +0500

    uncomment all string mothod's code

--- JavaScript Practice/4.Loops and Strings/strings.js
+++ JavaScript Practice/4.Loops and Strings/strings.js
@@ -16,23 +16,23 @@ let subj1 = 'Computer';
 let subj2 = 'Science';
 let word = subj1 + " " + subj2
 let wordList = word.split(" ");
-// // Concatenate
-// console.log(subj1 + subj2);
-// // embed expressions
-// console.log(`${subj1} ${subj2}`);
-// // to check length
-// console.log("Length of", subj1, "is:", subj1.length);
-// //uppercase and lowercase method
-// console.log(subj1.toUpperCase());
-// console.log(subj2.toLowerCase());
+// Concatenate
+console.log(subj1 + subj2);
+// embed expressions
+console.log(`${subj1} ${subj2}`);
+// to check length
+console.log("Length of", subj1, "is:", subj1.length);
+//uppercase and lowercase method
+console.log(subj1.toUpperCase());
+console.log(subj2.toLowerCase());
 //substrings
-// console.log(subj1.substring(1,5));
-// console.log(subj1.substring(1,7));
-// console.log(subj1.substring(1,));
-// console.log(subj1.substring(0,6));
-// console.log(subj1.substring(0,));
+console.log(subj1.substring(1,5));
+console.log(subj1.substring(1,7));
+console.log(subj1.substring(1,));
+console.log(subj1.substring(0,6));
+console.log(subj1.substring(0,));
 //split method
-// console.log(word.split(' '));
+console.log(word.split(' '));
 // join method
 console.log(wordList);
 console.log(wordList.join(" "));
