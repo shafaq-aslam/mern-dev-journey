50e1b3eaae161f8429ad201b5e1f1fe19de5e172
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-12 15:48:19 +0500

    uncomment all code of do while loop

--- JavaScript Practice/4.Loops and Strings/do_while_loop.js
+++ JavaScript Practice/4.Loops and Strings/do_while_loop.js
@@ -1,21 +1,21 @@
-// let i = 1;
-// do{
-//     if(i%2==0){
-//         console.log(i, "is divisible of 2");
-//     } else{
-//         console.log(i, "is not divisible of 2");
-//     }
-//     i++;
-// }while(i<=20);
+let i = 1;
+do{
+    if(i%2==0){
+        console.log(i, "is divisible of 2");
+    } else{
+        console.log(i, "is not divisible of 2");
+    }
+    i++;
+}while(i<=20);
 
-// let j = 1;
-// do{
-//     console.log(j);
-//     j++;
-// }while(j<4);
+let j = 1;
+do{
+    console.log(j);
+    j++;
+}while(j<4);
 
-// let k=1;
-// do{
-//     console.log("Good Mornig");
-//     k++;
-// }while(k>10);
+let k=1;
+do{
+    console.log("Good Mornig");
+    k++;
+}while(k>10);
