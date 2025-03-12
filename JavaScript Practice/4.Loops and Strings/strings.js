7a3dfc683f285f4074621e5882847f25e3fb3630
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-12 16:00:21 +0500

    strings code added

--- JavaScript Practice/4.Loops and Strings/strings.js
+++ JavaScript Practice/4.Loops and Strings/strings.js
@@ -1,2 +1,10 @@
 // Strings --> Sequence of charactors
 
+let str1 = 'Shafaq';
+console.log(str1, "\ntype is:", typeof(str1));
+let str2 = "Zeba is the Queen of house";
+console.log(str2, "\ntype is:", typeof(str2));
+let str3 = `Aslam is 
+the King 
+of house`;
+console.log(str3, "\ntype is:", typeof(str3));
