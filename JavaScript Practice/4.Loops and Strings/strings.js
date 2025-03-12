0bb0cd706ea87fc2a7a7b4064ddd9533dfab0807
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-12 16:16:47 +0500

    strings.js modified

--- JavaScript Practice/4.Loops and Strings/strings.js
+++ JavaScript Practice/4.Loops and Strings/strings.js
@@ -1,10 +1,25 @@
 // Strings --> Sequence of charactors
 
-let str1 = 'Shafaq';
-console.log(str1, "\ntype is:", typeof(str1));
-let str2 = "Zeba is the Queen of house";
-console.log(str2, "\ntype is:", typeof(str2));
-let str3 = `Aslam is 
-the King 
-of house`;
-console.log(str3, "\ntype is:", typeof(str3));
+// let str1 = 'Shafaq'; // It is primitive string
+// console.log(str1, "\ntype is:", typeof(str1));
+// let str2 = "Zeba is the Queen of house";
+// console.log(str2, "\ntype is:", typeof(str2));
+// let str3 = `Aslam is 
+// the King 
+// of house`;
+// console.log(str3, "\ntype is:", typeof(str3));
+
+// let firstName = new String("Shafaq Aslam"); // It creates string object
+// console.log(firstName, typeof(firstName));
+
+let subj1 = 'Computer';
+let subj2 = 'Science';
+// Concatenate
+console.log(subj1 + subj2);
+// embed expressions
+console.log(`${subj1} ${subj2}`);
+// to check length
+console.log("Length of", subj1, "is:", subj1.length);
+//uppercase and lowercase method
+console.log(subj1.toUpperCase());
+console.log(subj2.toLowerCase());
