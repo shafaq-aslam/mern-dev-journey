8da77bcfe908eee444675397080663b04fc933d9
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-13 15:47:39 +0500

    functions file updated

--- JavaScript Section/5.Plan and Arrow Functions/function_1.js
+++ JavaScript Section/5.Plan and Arrow Functions/function_1.js
@@ -59,3 +59,10 @@ function printGreetings(fName, lName){
 }
 
 console.log(printGreetings("Zainab", "Zaheer"));
+
+//another way to write function
+let getAverage = function(num1, num2){
+    return (num1 + num2)/getAverage.length;
+}
+
+console.log(getAverage(5, 15));
