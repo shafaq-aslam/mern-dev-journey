21ca01a521bf3898c4cc896f179468fe3f95becd
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-13 15:56:59 +0500

    function file 2 modified

--- JavaScript Section/5.Plan and Arrow Functions/function_2.js
+++ JavaScript Section/5.Plan and Arrow Functions/function_2.js
@@ -4,3 +4,12 @@ let getAverage = function(num1, num2){
 }
 
 console.log(getAverage(5, 15));
+
+//print table
+let printTable = function(table, startPoint, endPoint){
+    for(let i=startPoint; i<=endPoint; i++){
+        console.log(table, "x", i, "=", table*i);
+    }
+}
+
+printTable(5, 2, 15);
