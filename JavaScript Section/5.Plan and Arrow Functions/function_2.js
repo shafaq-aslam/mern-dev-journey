70cbfc486df849b3860c2406afbf3ea7f824f821
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-13 16:13:38 +0500

    function 2 file modified

--- JavaScript Section/5.Plan and Arrow Functions/function_2.js
+++ JavaScript Section/5.Plan and Arrow Functions/function_2.js
@@ -1,18 +1,18 @@
-// //another way to write function
-// let getAverage = function(num1, num2){
-//     return (num1 + num2)/getAverage.length;
-// }
+//Expression function
+let getAverage = function(num1, num2){
+    return (num1 + num2)/getAverage.length;
+}
 
-// console.log(getAverage(5, 15));
+console.log(getAverage(5, 15));
 
-// //print table
-// let printTable = function(table, startPoint, endPoint){
-//     for(let i=startPoint; i<=endPoint; i++){
-//         console.log(table, "x", i, "=", table*i);
-//     }
-// }
+//print table
+let printTable = function(table, startPoint, endPoint){
+    for(let i=startPoint; i<=endPoint; i++){
+        console.log(table, "x", i, "=", table*i);
+    }
+}
 
-// printTable(5, 2, 15);
+printTable(5, 2, 15);
 
 //Arrow function
 let getSquare = (number) => {
