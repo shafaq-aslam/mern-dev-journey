588eed744683c53e3533ddf7c4adcd500b4f2c7e
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-14 22:25:55 +0500

    modified

--- JavaScript Section/6.Arrays/array_1.js
+++ JavaScript Section/6.Arrays/array_1.js
@@ -1,5 +1,10 @@
-//Reference types are:
+// Reference types are:
 // Object, Array, Function
 
 // All reference types created in heep memory
 // but for access in heep memory we need a pointer/reference which created in stack memory.
+
+// What is Object?
+// It is just the collection of key value pairs.
+
+// let obj = {key: value, key: value} ---> Syntax
