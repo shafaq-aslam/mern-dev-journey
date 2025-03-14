994c2fb761193c4dfb515318f9b693e6dfb2638b
shafaq-aslam <shafaqchohan123@gmail.com>
2025-03-14 22:21:56 +0500

    array file created

---
+++ JavaScript Section/6.Arrays/array_1.js
@@ -0,0 +1,5 @@
+//Reference types are:
+// Object, Array, Function
+
+// All reference types created in heep memory
+// but for access in heep memory we need a pointer/reference which created in stack memory.
